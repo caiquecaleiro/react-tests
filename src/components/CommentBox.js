432cb166baf72282415520d534fc81a27ba7a219
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-19 19:02:23 -0200

    Add the setup for the tests and implemented some tests for App and CommentBox components

---
+++ src/components/CommentBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CommentBox() {
+  return (
+    <div className="comment-box">
+      <textarea />
+      <button>Test</button>
+    </div>
+  );
+}
+
+export default CommentBox;
